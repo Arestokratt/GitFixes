ab117a4cc5baaead24dffd13a93a8fdaa35ae348
Arestokratt <steinhauergalaov@gmail.com>
2026-04-09 12:06:22 +0300

    Add "HALLO" мессадже

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("HALLOOOO EVERYYONE");
+
+        System.out.println("AND HALLO AGAIN EVERYONEREEEEE");
     }
 }
